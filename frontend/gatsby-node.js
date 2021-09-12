45174f2c197cc4036cbd4daa07420436c9ab4a21
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2021-09-12 11:26:11 +0100

    typescript added, eslint, stylelint to structure

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@images': path.resolve(__dirname, 'src/images'),
+        '@fonts': path.resolve(__dirname, 'src/fonts')
+      },
+    },
+  })
+}
